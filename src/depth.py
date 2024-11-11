f055032e731169b81ebb8d22043762b3bcc3a3b2
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-11-11 16:17:33 +0100

    fix: pcd visualization

--- src/depth.py
+++ src/depth.py
@@ -1,3 +1,5 @@
+import threading
+import time
 from typing import Union, Iterator, Tuple
 
 import cv2
@@ -7,19 +9,32 @@ from numpy import ndarray
 
 from load import load_stereo_images
 
+center = None
 
-def draw_registrations(cloud, transformation=None):
-    if transformation is not None:
-        cloud.transform(transformation)
 
-    # TODO dynamic
-    o3d.visualization.draw_geometries([cloud])
+def __show_pcd__(cloud, should_show):
+    global center
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width=800, height=600)
+    vis.add_geometry(cloud)
+    view_control = vis.get_view_control()
+    view_control.set_zoom(0.17)
+    if center is None:
+        center = cloud.get_center() + np.array([-7., 0., 0.])
+    view_control.set_lookat(center)
+
+    while not should_show.is_set():  # Keep running until stop_event is set
+        vis.poll_events()
+        vis.update_renderer()
+        time.sleep(0.01)
+    vis.destroy_window()
 
 
 def init_stereo(use_sgbm=True) -> Union[cv2.StereoSGBM, cv2.StereoBM]:
     if use_sgbm:
         window_size = 3
-        min_disp = 16
+        min_disp = 6
         num_disp = 112 - min_disp
         # TODO tweak params
         return cv2.StereoSGBM_create(minDisparity=min_disp,
@@ -86,7 +101,7 @@ def depth_to_pcd(image_left: ndarray, disparity: ndarray, scale: float = 1) -> o
     mask_map = disparity > disparity.min()
 
     pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(points_3d[mask_map])
+    pcd.points = o3d.utility.Vector3dVector(points_3d[mask_map].astype(np.float64))
     pcd.colors = o3d.utility.Vector3dVector(colors[mask_map].astype(np.float64) / 255.0)
     return pcd
 
@@ -110,11 +125,28 @@ def main(sequence):
     stereo = init_stereo(use_sgbm=True)
 
     for i, (rec_left, rec_right, disparity, pcd) in enumerate(get_stereo_image_disparity_pcd(sequence, stereo)):
-        cv2.imshow("rec", cv2.resize(disparity / disparity.max(), None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR))
-        draw_registrations(pcd)
+        cv2.imshow("left and right",
+                   cv2.resize(np.concatenate([rec_left, rec_right], axis=1),
+                              None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR))
+        cv2.imshow("disparity",
+                   cv2.resize(disparity / disparity.max(),
+                              None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR))
+        cv2.imshow("disparity used",
+                   cv2.resize(np.ones_like(disparity) * (disparity > disparity.min()),
+                              None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR))
 
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        cloud = pcd.voxel_down_sample(voxel_size=0.05)
+
+        should_show = threading.Event()
+        pcd_thread = threading.Thread(target=__show_pcd__, args=(cloud, should_show))
+        pcd_thread.start()
+
+        k = cv2.waitKey(0)
+        should_show.set()
+        pcd_thread.join()
+        if k & 0xFF == ord('q'):
             break
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
