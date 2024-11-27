f4945b929b4e9a7b00d8f3dbc9b333e736973b2f
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-11-27 15:30:51 +0100

    chore: reformat kalman_filter.py

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -1,35 +1,37 @@
 import numpy as np
-import cv2
-with_prev=True
+
+with_prev = True
+
+
 class KalmanFilter2D:
     def __init__(self, dt=1):
         self.dt = dt
         if with_prev:
             self.F = np.array([[1, dt, 0, 0, 0, 0],  # x_pos
-                            [1, 0, 0, 0, -1, 0],  # x_vel
-                            [0, 0, 1, dt, 0 , 0], # y_pos
-                            [0, 0, 1, 0, 0, -1],  # y_vel
-                            [1, 0, 0, 0, 0, 0],  # x_prev_pos
-                            [0, 0, 1, 0, 0, 0]]) # y_prev_pos
+                               [1, 0, 0, 0, -1, 0],  # x_vel
+                               [0, 0, 1, dt, 0, 0],  # y_pos
+                               [0, 0, 1, 0, 0, -1],  # y_vel
+                               [1, 0, 0, 0, 0, 0],  # x_prev_pos
+                               [0, 0, 1, 0, 0, 0]])  # y_prev_pos
             self.H = np.array([[1, 0, 0, 0, 0, 0],  # Measurement matrix
-                            [0, 0, 1, 0, 0, 0]])
+                               [0, 0, 1, 0, 0, 0]])
             self.R = np.eye(2) * 1000
             self.I = np.eye(6)  # Identity matrix
         else:
             self.F = np.array([[1, dt, 0, 0],  # x_pos
-                            [0, 1, 0, 0],  # x_vel
-                            [0, 0, 1, dt], # y_pos
-                            [0, 0, 0, 0]])  # y_vel
+                               [0, 1, 0, 0],  # x_vel
+                               [0, 0, 1, dt],  # y_pos
+                               [0, 0, 0, 0]])  # y_vel
             self.H = np.array([[1, 0, 0, 0],  # Measurement matrix
-                            [0, 0, 1, 0]])
+                               [0, 0, 1, 0]])
             self.R = np.eye(2) * 1000
             self.I = np.eye(4)  # Identity matrix
 
-    def initialize(self, x_pos = 0, y_pos = 0):
+    def initialize(self, x_pos=0, y_pos=0):
         if with_prev:
             x = np.zeros((6, 1))  # Initial state [x, vx, y, vy, x_prev, y_prev]
             x = np.array([[x_pos], [0], [y_pos], [0], [x_pos], [y_pos]])  # Initial state
-            P = np.eye(6) * 1000 # Initial state covariance
+            P = np.eye(6) * 1000  # Initial state covariance
             return x, P
         else:
             x = np.zeros((4, 1))  # Initial state [x, vx, y, vy, x_prev, y_prev]
@@ -54,20 +56,21 @@ class KalmanFilter2D:
         x = np.dot(self.F, x) + u  # Predicted state
         P = np.dot(self.F, np.dot(P, self.F.T))  # Predicted covariance
         return x, P
-    
+
+
 class KalmanFilterManager:
     def __init__(self):
         self.kalman_filters = {}
 
     def initialize_filters(self, detections):
-            for det in detections:
-                class_id, x, y, w, h = det
-                if class_id not in self.kalman_filters:
-                    self.kalman_filters[class_id] = {}
-                obj_id = f"{class_id}_{len(self.kalman_filters[class_id])}"
-                kf = KalmanFilter2D()
-                kf.x, kf.P = kf.initialize(x, y)
-                self.kalman_filters[class_id][obj_id] = kf
+        for det in detections:
+            class_id, x, y, w, h = det
+            if class_id not in self.kalman_filters:
+                self.kalman_filters[class_id] = {}
+            obj_id = f"{class_id}_{len(self.kalman_filters[class_id])}"
+            kf = KalmanFilter2D()
+            kf.x, kf.P = kf.initialize(x, y)
+            self.kalman_filters[class_id][obj_id] = kf
 
     def update(self, detections):
         for class_id, filters in self.kalman_filters.items():
@@ -96,7 +99,7 @@ class KalmanFilterManager:
 
             for obj_id, pred in predictions.items():
                 pred_x, pred_y = pred[0, 0], pred[2, 0]
-                if len(measurements) == 0: # if we dont find a measurement for a prediction, we dont update the prediction
+                if len(measurements) == 0:  # if we dont find a measurement for a prediction, we dont update the prediction
                     break
                 closest_measurement = min(measurements, key=lambda m: np.linalg.norm((pred_x - m[0], pred_y - m[1])))
                 measurements.remove(closest_measurement)
@@ -117,7 +120,7 @@ class KalmanFilterManager:
     def get_class_counts(self):
         class_counts = {class_id: len(filters) for class_id, filters in self.kalman_filters.items()}
         return class_counts
-    
+
     def get_predictions_list(self):
         result = []
         predictions = self.predict()
