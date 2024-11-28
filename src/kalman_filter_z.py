f22278756391d1680adff7ae16de388c0479adf5
Nandor Takacs <nandortakacs123@gmail.com>
2024-11-28 21:10:31 +0100

    kalman filter update

--- src/kalman_filter_z.py
+++ src/kalman_filter_z.py
@@ -32,8 +32,10 @@ class KalmanFilter2D:
         self.P = np.dot(self.F, np.dot(self.P, self.F.T))  # Predicted covariance
     
 class KalmanFilterManager:
-    def __init__(self):
+    def __init__(self, frame_width= 370, frame_height= 1224):
         self.kalman_filters = {}
+        self.frame_height = frame_height
+        self.frame_width = frame_width
 
     def initialize_filters(self, measurements):
             for mes in measurements:
@@ -46,39 +48,42 @@ class KalmanFilterManager:
 
     def update(self, measurements):
         for class_id, filters in self.kalman_filters.items():
-            can_continue = False
-            for mes in measurements:
-                if mes[0] == class_id:
-                    can_continue = True
-                    break
-            if not can_continue:
-                continue
-
-            # Get predictions
+            # Get predictions for the current class
             predictions = {}
             for obj_id, kf in filters.items():
                 kf.predict()
                 predictions[obj_id] = kf.x
 
-            # Pair predictions with closest measurements
-            obj_locations = []
-            for mes in measurements:
-                if mes[0] == class_id:
-                    x, y, z = mes[1], mes[2], mes[3]
-                    obj_locations.append((x, y, z))
-            if len(obj_locations) == 0:
-                continue
+            # Get measurements for the current class
+            mes_obj_locations = self.get_mes_obj_locations(measurements, class_id)
 
+
+            # Remove filters with predictions out of frame
+            filters_to_remove = self.get_out_of_frame_predictions(predictions)
+            if filters_to_remove is not None:
+                for obj_id in filters_to_remove:
+                    predictions.pop(obj_id)
+                    del filters[obj_id]
+          
+            # Update filters with measurements closest to them and within a threshold
             for obj_id, pred in predictions.items():
-                pred_x, pred_y, pred_z = pred[0, 0], pred[2, 0], pred[4, 0]
-                if len(obj_locations) == 0: # if we dont find a measurement for a prediction, we dont update the prediction
+                if len(mes_obj_locations) == 0:
                     break
-                closest_obj = min(obj_locations, key=lambda m: np.linalg.norm((pred_x - m[0], pred_y - m[1], pred_z - m[2])))
-                obj_locations.remove(closest_obj)
-                mes_for_kf = np.array([[closest_obj[0]], [closest_obj[1]], [closest_obj[2]]])
-                kf = filters[obj_id]
-                kf.update(mes_for_kf)
-                print("updated")
+                mes_for_kf = self.find_closest_measurement(pred, mes_obj_locations)
+                if mes_for_kf is not None:
+                    kf = filters[obj_id]
+                    kf.update(mes_for_kf)
+                    print("updated")
+                else:
+                    print("not updated")
+
+            # Initialize new filters for those measurements that are close to the frame boundary
+            if mes_obj_locations:
+                filter_input=[]
+                for (x, y, z) in mes_obj_locations:
+                    if x < 20 or x > self.frame_width - 20 or y < 20 or y > self.frame_height - 20:
+                        filter_input.append([class_id, x, y, z])
+                self.initialize_filters(filter_input)
 
     def predict(self):
         predictions = {}
@@ -88,7 +93,35 @@ class KalmanFilterManager:
                 kf.predict()
                 predictions[class_id][obj_id] = np.dot(kf.H, kf.x)
         return predictions
+    
+    def get_out_of_frame_predictions(self, predictions):
+        out_of_frame_filters = []
+        treshold = 0
+        for obj_id, pred in predictions.items():
+            x, y, z = pred[0, 0], pred[2, 0], pred[4, 0]
+            if x < treshold or x > self.frame_height-treshold or y < treshold or y > self.frame_width - treshold:
+                out_of_frame_filters.append(obj_id)
+        return out_of_frame_filters
 
+    def find_closest_measurement(self, pred, mes_obj_locations):
+        pred_x, pred_y, pred_z = pred[0, 0], pred[2, 0], pred[4, 0]
+        closest_mes_obj = min(mes_obj_locations, key=lambda m: np.linalg.norm((pred_x - m[0], pred_y - m[1], pred_z - m[2])))
+        distance = np.linalg.norm((pred_x - closest_mes_obj[0], pred_y - closest_mes_obj[1], pred_z - closest_mes_obj[2]))
+        threshold = 50  # Define your threshold value here
+        if distance > threshold:
+            return None  
+        mes_obj_locations.remove(closest_mes_obj)
+        mes_for_kf = np.array([[closest_mes_obj[0]], [closest_mes_obj[1]], [closest_mes_obj[2]]])
+        return mes_for_kf
+
+    def get_mes_obj_locations(self, measurements, class_id):
+        mes_obj_locations = []
+        for mes in measurements:
+            if mes[0] == class_id:
+                x, y, z = mes[1], mes[2], mes[3]
+                mes_obj_locations.append((x, y, z))
+        return mes_obj_locations
+    
     def get_class_counts(self):
         class_counts = {class_id: len(filters) for class_id, filters in self.kalman_filters.items()}
         return class_counts
