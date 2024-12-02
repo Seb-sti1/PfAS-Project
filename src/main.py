d73fc2f64d933e1151fdd2645880131eff51e4a1
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-12-02 10:27:51 +0100

    feat: final adjustment

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from ultralytics import YOLO
 from depth import init_stereo, get_disparity, baseline, R_rect, get_roi_xyz
 from detect import detect_objects
 from kalman_filter_z import KalmanFilterManager
-from load import load_stereo_images
+from load import load_stereo_images, load_labels
+from viz import add_labels
 
 K = np.array([[925.46, 0, 702.68], [0, 938.2, 259.91], [0, 0, 1]])
 D = np.array([[-0.36083, 0.14103, -0.00837, 0.00406, 0.00562]])
@@ -26,6 +27,11 @@ def draw_rectangle(img, class_id, x_p, y_p, w, h, colors):
     cv2.rectangle(img, (x_p - w // 2, y_p - h // 2), (x_p + w // 2, y_p + h // 2), colors[class_id], 2)
 
 
+def custom_imshow(title, image, resize=0.6):
+    cv2.imshow(title, cv2.resize(image, None,
+                                 fx=resize, fy=resize, interpolation=cv2.INTER_LINEAR))
+
+
 def undistorted(img: np.ndarray, K, D):
     h, w = img.shape[:2]
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(K, D, (w, h), 1, (w, h))
@@ -41,7 +47,7 @@ def xyz_from_roi_center_and_z(center: Tuple[int, int], z: float, f: float) -> Tu
     return x_p * z / f, y_p * z / f, z
 
 
-def interactive(seq):
+def everything_homemade(seq):
     # parameters for stereo
     stereo = init_stereo(use_sgbm=True)
     _, _, _, _, Q, _, _ = cv2.stereoRectify(K, D, K, D, S_rect, R_rect, np.array([-baseline, 0., 0.]))
@@ -54,13 +60,15 @@ def interactive(seq):
     initial_pred = True
 
     # display params
+    labels_pd = load_labels("rec_data", seq) if seq != "seq_03" else None
     colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    name_to_index = {"Pedestrian": 0, "Cyclist": 1, "Car": 2}
 
-    for (raw_left, raw_right), (true_rec_left, true_rec_right) in zip(load_stereo_images("raw_data", seq),
-                                                                      load_stereo_images("rec_data", seq)):
+    for index, ((raw_left, raw_right), (true_rec_left, true_rec_right)) in enumerate(
+            zip(load_stereo_images("raw_data", seq),
+                load_stereo_images("rec_data", seq))):
         # rectify using calibration
         rec_left, rec_right = undistorted(raw_left, K, D), undistorted(raw_right, K, D)
-        # rec_left, rec_right = true_rec_left, true_rec_right
 
         # send rec_left to YOLO for detection
         raw_measured_roi_list = detect_objects(model, rec_left)
@@ -88,20 +96,23 @@ def interactive(seq):
                            for (class_id, x_p, y_p, z, _, _) in kf_manager.get_predictions_list()]
 
         # display (true and estimated)
-        cv2.imshow("raw_left", raw_left)
+        custom_imshow("raw_left", raw_left)
 
         yolo_rec_left = rec_left.copy()
         for pred in raw_measured_roi_list:
             draw_rectangle(yolo_rec_left, *pred, colors)
-        cv2.imshow("yolo detection on rec_left", yolo_rec_left)
+        custom_imshow("yolo detection on rec_left", yolo_rec_left)
 
-        cv2.imshow("disparity", disparity / disparity.max())
+        custom_imshow("disparity", disparity / disparity.max())
 
         for pred in prediction_list:
             draw_circle(rec_left, *pred, colors)
-        cv2.imshow("rec_left", rec_left)
+        custom_imshow("rec_left", rec_left, resize=1)
 
-        cv2.imshow("true_rec_left", true_rec_left)
+        if labels_pd is not None:
+            current_labels = labels_pd[labels_pd["frame"] == index]
+            add_labels(true_rec_left, current_labels, {name: colors[value] for name, value in name_to_index.items()})
+        custom_imshow("true_rec_left", true_rec_left, resize=1)
         if cv2.waitKey(0) & 0xFF == ord('q'):
             break
 
@@ -112,7 +123,7 @@ def main(args=None):
                         choices=["seq_01", "seq_02", "seq_03"], default="seq_01")
     args = parser.parse_args()
 
-    interactive(args.sequence)
+    everything_homemade(args.sequence)
 
 
 if __name__ == "__main__":
